dd9081bde453b66c91414c85dfadb408cbe6f23c
sagudev <samo.golez@outlook.com>
2021-06-05 14:06:59 +0200

    r1

---
+++ src/bin.rs
@@ -0,0 +1,10 @@
+#[cfg(all(target_os = "linux", target_arch = "x86"))]
+static BIN_FILE: &'static [u8] = include_bytes!("../bin/l32/LAC");
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+static BIN_FILE: &'static [u8] = include_bytes!("../bin/l64/LAC");
+#[cfg(all(target_os = "windows", target_arch = "x86"))]
+static BIN_FILE: &'static [u8] = include_bytes!("../bin/win32/LAC.exe");
+#[cfg(all(target_os = "windows", target_arch = "x86_64"))]
+static BIN_FILE: &'static [u8] = include_bytes!("../bin/win64/LAC.exe");
+#[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+static BIN_FILE: &'static [u8] = include_bytes!("../bin/mac64/LAC");
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use llv;
+
 fn main() {
     println!("Hello, world!");
 }
